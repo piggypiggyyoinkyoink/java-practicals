a703af283d8db7d71d5a5f8d75699b4328cfe4a4
piggypiggyyoinkyoink <quackquack904@gmail.com>
2026-02-27 15:41:53 +0000

    Exercise 2
    .

--- practical2/src/Main.java
+++ practical2/src/Main.java
@@ -13,6 +13,37 @@ class Employee{
         System.out.println("Name: "+this.name+", Salary: £"+this.salary+", Company: "+company);
     }
 }
+class BankAccount{
+    static int totalAccounts = 0;
+    private int accountNumber;
+    private double balance;
+    public BankAccount(double balance){
+        if (balance < 0){
+            balance = 0;
+        }
+        this.balance = balance;
+        totalAccounts++;
+        this.accountNumber = totalAccounts;
+    }
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+    public void withdraw(double amount){
+        if (amount > this.balance){
+            return;
+        } else{
+            this.balance -= amount;
+        }
+    }
+    public void displayInfo(){
+        System.out.println("Account number: "+this.accountNumber+", Balance: £"+this.balance);
+    }
+    public static void displayTotalAccounts(){
+        System.out.println("Total number of accounts: "+totalAccounts);
+    }
+
+}
+
 
 public class Main {
     public static void main(String[] args) {
@@ -22,5 +53,14 @@ public class Main {
         Employee obama = new Employee("Obama", 10000000);
         joseph.displayInfo();
         obama.displayInfo();
+
+        // Exercise 2
+        BankAccount rich = new BankAccount(999999);
+        BankAccount broke = new BankAccount(0.01);
+        rich.deposit(1000);
+        broke.withdraw(5);
+        rich.displayInfo();
+        broke.displayInfo();
+        BankAccount.displayTotalAccounts();
     }
 }
